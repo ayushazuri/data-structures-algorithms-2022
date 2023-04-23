ff601d7357738e49bee18883d74703d93bf58fc8
ayushsingh <ayushazuri@gmail.com>
2023-04-23 21:57:29 +0530

    Striver a2z - Complex pattern

---
+++ striver-a2z-dsa/basics/src/Pattern.java
@@ -0,0 +1,37 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 5;
+        buildPattern(n);
+        buildInvertPattern(n);
+    }
+
+    private static void buildInvertPattern(int n) {
+        for(int i = n; i >= 1 ; i--){
+            for(int j = n-1; j >= i;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j <= i - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void buildPattern(int n) {
+        for(int i = 1; i <= n ; i++){
+            for(int j = n-1; j >= i;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j <= i - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
