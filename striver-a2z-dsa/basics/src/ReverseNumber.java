fe44ce60ccf4bb2244d5b9c7724efe900c60d15e
ayushsingh <ayushazuri@gmail.com>
2023-04-28 01:04:53 +0530

    Striver a2z - Few Basic Questions

---
+++ striver-a2z-dsa/basics/src/ReverseNumber.java
@@ -0,0 +1,20 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int n = 123100;
+        reverseNumber(n);
+    }
+
+    private static void reverseNumber(int n) {
+        int res = 0;
+        while(n != 0){
+            if(res == 0 && n % 10 == 0)
+                n = n/10;
+            else {
+                res = res * 10 + (n % 10);
+                n = n/10;
+            }
+
+        }
+        System.out.println(res);
+    }
+}
