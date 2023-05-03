333d1d9c21365cd5ea93f1c1418e8010a80fd154
ayushsingh <ayushazuri@gmail.com>
2023-05-04 00:16:32 +0530

    Striver a2z - Selection Sort

---
+++ striver-a2z-dsa/sorting/src/SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] a = new int[]{13, 2, 34, 31, 22, 20};
+        selectionSort(a);
+    }
+
+    private static void selectionSort(int[] a) {
+        if(a.length == 0) return;
+
+        for(int i = 0; i < a.length; i++){
+            int min = i;
+            for(int j = i + 1; j < a.length; j++){
+                if(a[j] < a[i]){
+                    min = j;
+                }
+            }
+            int t = a[i];
+            a[i] = a[min];
+            a[min] = t;
+        }
+
+        Arrays.stream(a).forEach(System.out::println);
+    }
+}
