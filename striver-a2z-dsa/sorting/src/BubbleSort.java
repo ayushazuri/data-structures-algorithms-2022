b95ce334def9eb616d4205327d1a7c2d99d6e478
ayushsingh <ayushazuri@gmail.com>
2023-05-04 23:30:33 +0530

    Striver a2z - Bubble Sort

---
+++ striver-a2z-dsa/sorting/src/BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] a = new int[]{13, 2, 34, 31, 22, 20};
+        bubbleSort(a);
+    }
+
+    private static void bubbleSort(int[] a) {
+        for(int i = 0;i < a.length;i++){
+            boolean isSorted = true;
+
+            for(int j = i + 1; j < a.length;j++){
+                if(a[j - 1] > a[j]){
+                    int t = a[j-1];
+                    a[j-1] = a[j];
+                    a[j] = t;
+                    isSorted = false;
+                }
+            }
+            if(isSorted)
+                break;
+        }
+
+        Arrays.stream(a).forEach(System.out::println);
+    }
+}
