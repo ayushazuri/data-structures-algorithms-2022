9ca2a03e4a8c4359a6132f1bbff010c03c9d0252
ayushsingh <ayushazuri@gmail.com>
2023-07-10 03:29:55 +0530

    Striver a2z - Array

---
+++ striver-a2z-dsa/src/main/java/org/coding/array/IsSortedArray.java
@@ -0,0 +1,32 @@
+package org.coding.array;
+
+public class IsSortedArray {
+    public static void main(String[] args) {
+        int[] a = {2, 4, 10, 2, 5, 7};
+        System.out.println(isSortedArray(a));
+        int[] b = {1, 2, 30, 99};
+        System.out.println(isSortedArray(b));
+        int[] c = {1, 1, 1};
+        System.out.println(isSortedArray(c));
+    }
+
+    private static boolean isSortedArray(int[] a) {
+        if( a.length == 0 || a.length == 1)
+            return true;
+
+        boolean asc = a[0] < a[1];
+
+        if(asc){
+            for(int i = 1;i < a.length;i++){
+                if(a[i] < a[i-1])
+                    return false;
+            }
+        } else {
+            for(int i = 1;i < a.length;i++){
+                if(a[i] > a[i-1])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ striver-a2z-dsa/src/main/java/org/coding/array/RemoveDupeFromSortedArr.java
@@ -0,0 +1,28 @@
+package org.coding.array;
+
+public class RemoveDupeFromSortedArr {
+    public static void main(String[] args) {
+        int[] a = {0,0,1,1,1,2,2,3,3,4};
+        int[] k = {0, 1, 2, 3, 4};
+        int i = removeDuplicates(a);
+
+        for(int j = 0; j < i; j++){
+            if(a[j] != k[j])
+                System.out.println(false);
+        }
+
+    }
+
+    static int removeDuplicates(int[] a) {
+        if(a.length == 0) return 0;
+
+        int j = 0;
+        for (int i = 1; i < a.length; i++){
+            if(a[i] != a[i-1]){
+                j++;
+                a[j] = a[i];
+            }
+        }
+        return j+1;
+    }
+}
